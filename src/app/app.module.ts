d542ce9a92e7012f4325a21b4362f9d61abebf60
lm1 <lukasz.majcher@softnet.com.pl>
2020-03-24 14:17:09 +0100

    add flex layout and tasks view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatListModule} from '@angular/material/list';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import {MatRadioModule} from '@angular/material/radio';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,9 @@ import {MatListModule} from '@angular/material/list';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatListModule
+    MatListModule,
+    FlexLayoutModule,
+    MatRadioModule
   ],
   providers: [],
   bootstrap: [AppComponent]
